f5f295036438ee0655ff3481f31ada9280a100a7
danagilliann <danalee96@gmail.com>
2015-08-15 15:52:59 -0400

    added layout, show_entries, and login to template

--- flaskr.py
+++ flaskr.py
@@ -41,11 +41,33 @@ def show_entries():
 
 @app.route('/add', method=['POST'])
 def add_entry():
-	if not session.get('logged_in'):
+	if not session.get('logged_in'): # 'logged_in' looks for user's key
 		abort(401)
 	g.db.execute('insert into entries (title, text) values (?, ?)', 
 		[request.form['title'], request.form['text']])
+	g.db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries')) 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'	
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return reirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
 	app.run()
